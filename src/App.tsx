ced8edf0d535d1aad1374fe56505263a6d6570aa
신준혁 <oridori2705@naver.com>
2024-01-19 21:01:53 +0900

    build: TanStack Query 환경 구축

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,38 @@ import useTheme from './styles/useTheme'
 import GlobalStyle from './styles/GlobalStyles'
 import { default as THEME } from './styles/Theme'
 import ThemeSwitch from './components/ThemeSwitch'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 5,
+      retry: 0,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 function App() {
   const [theme, onToggle] = useTheme()
 
   return (
     <>
-      <ThemeProvider theme={THEME[theme]}>
-        <Global styles={GlobalStyle(THEME[theme])} />
-        <MainContainer>
-          <ThemeSwitch
-            checked={theme === 'dark'}
-            toggleSwitch={onToggle}
-          />
-          <ProfileContainer>dsadsdadsasda</ProfileContainer>
-        </MainContainer>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={THEME[theme]}>
+          <Global styles={GlobalStyle(THEME[theme])} />
+          <MainContainer>
+            <ThemeSwitch
+              checked={theme === 'dark'}
+              toggleSwitch={onToggle}
+            />
+            <ProfileContainer>나는 글씨다!</ProfileContainer>
+          </MainContainer>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   )
 }
