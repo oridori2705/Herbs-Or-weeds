07e5303692e25f37587e56c4efcda7650e92451e
신준혁 <oridori2705@naver.com>
2024-01-19 22:20:44 +0900

    build: react-router-dom 설치 및 환경 구축

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import styled from '@emotion/styled'
 import useTheme from './styles/useTheme'
 import GlobalStyle from './styles/GlobalStyles'
 import { default as THEME } from './styles/Theme'
-import ThemeSwitch from './components/ThemeSwitch'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/Router'
+import ThemeSwitch from './components/ThemeSwitch'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,19 +22,18 @@ const queryClient = new QueryClient({
 
 function App() {
   const [theme, onToggle] = useTheme()
-
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={THEME[theme]}>
           <Global styles={GlobalStyle(THEME[theme])} />
           <MainContainer>
-            <ThemeSwitch
-              checked={theme === 'dark'}
-              toggleSwitch={onToggle}
-            />
-            <ProfileContainer>나는 글씨다!</ProfileContainer>
+            <RouterProvider router={router} />
           </MainContainer>
+          <ThemeSwitch
+            checked={theme === 'dark'}
+            toggleSwitch={onToggle}
+          />
           <ReactQueryDevtools initialIsOpen={false} />
         </ThemeProvider>
       </QueryClientProvider>
@@ -45,7 +46,4 @@ export default App
 const MainContainer = styled.div`
   margin: 0 auto;
   max-width: 780px;
-`
-const ProfileContainer = styled.div`
-  margin: 1rem;
 `
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <section>
+      <div>나는 글씨다!</div>
+    </section>
+  )
+}
