7fcd3fc1aaf7c135bb1c1ccab7eed1b6271f8d7e
신준혁 <oridori2705@naver.com>
2024-03-28 18:27:49 +0900

    fix: build 오류 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import App from './App.tsx'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/Router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
