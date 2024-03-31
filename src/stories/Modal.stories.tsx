2192e5104001984fe946256353258f6313caa2f8
신준혁 <oridori2705@naver.com>
2024-03-31 16:18:33 +0900

    feat: useModal 스토리북 추가

---
+++ src/stories/Modal.stories.tsx
@@ -0,0 +1,27 @@
+import { Global, ThemeProvider, useTheme } from "@emotion/react";
+import useModal from "~/hooks/useModal";
+import GlobalStyle from "~/styles/GlobalStyles";
+import { default as THEME } from '~/styles/Theme'
+
+export default {
+    title: 'Common/Components/useModal',
+} 
+
+export const Default = {
+    render: function Render() {
+        const {Modal,open,close,isOpen} = useModal();
+
+    return (
+        <div>
+            <ThemeProvider theme={THEME["dark"]}>
+            <Global styles={GlobalStyle(THEME["dark"])} />
+                <button onClick={open} style={{backgroundColor:"lightblue", padding: "5px", borderRadius: "5px"}}>Modal 오픈</button>
+                <Modal isOpen={isOpen} close={close}>
+                    <div>안녕하세요</div>
+                    <div>하이하이복실</div>
+                </Modal>
+            </ThemeProvider>
+        </div>
+    )
+    }
+}
