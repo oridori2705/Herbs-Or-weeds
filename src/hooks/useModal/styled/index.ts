6e6f9809cfe26330b44d990147d1bc5d712f5e11
신준혁 <90139306+oridori2705@users.noreply.github.com>
2024-04-01 19:33:27 +0900

    refactor: 조건부 렌더링 및 transition을 위한 setTimeout 추가 (#25)
    * refactor: 조건부 렌더링 및 transition을 위한 setTimeout 추가
    
    * refactor: 다중 Modal 사용 시 식별이 잘 되도록 변경
    
    * refactor: useModal 리팩토링으로 인한 코드 누락 수정

--- src/hooks/useModal/styled/index.ts
+++ src/hooks/useModal/styled/index.ts
@@ -4,6 +4,11 @@ interface ModalOverlayAndContentProps {
   isOpen: boolean
 }
 
+interface ModalContentProps extends ModalOverlayAndContentProps {
+  width: number
+  height: number
+}
+
 export const ModalOverlay = styled.div<ModalOverlayAndContentProps>`
   position: fixed;
   top: 0;
@@ -13,28 +18,26 @@ export const ModalOverlay = styled.div<ModalOverlayAndContentProps>`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
-  z-index: 40;
+  z-index: 50;
   transition: opacity 1s ease;
-  opacity: ${props => (props.isOpen ? 100 : 0)};
-  transform: translateX(${props => (props.isOpen ? 0 : '100vw')});
+  opacity: ${props => (props.isOpen ? 0.99 : 0)};
 `
 
-export const ModalContent = styled.div<ModalOverlayAndContentProps>`
+export const ModalContent = styled.div<ModalContentProps>`
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  width: 70%;
-  height: 70%;
+  width: ${props => `${props.width}%`};
+  height: ${props => `${props.height}%`};
   margin: auto;
   border-radius: 0.5rem;
   background-color: white;
-  z-index: 40;
+  z-index: 50;
   color: black;
   overflow: scroll;
   -ms-overflow-style: none;
-
   scrollbar-width: none;
   transform: translateY(${props => (props.isOpen ? 0 : '100vh')});
   transition: transform 1s ease;
