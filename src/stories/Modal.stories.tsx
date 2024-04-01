bb9003f12b0b96d0a65cebe12a82404af9323573
신준혁 <oridori2705@naver.com>
2024-04-01 19:32:05 +0900

    refactor: useModal 리팩토링으로 인한 코드 누락 수정

--- src/stories/Modal.stories.tsx
+++ src/stories/Modal.stories.tsx
@@ -1,27 +1,38 @@
-import { Global, ThemeProvider } from "@emotion/react";
-import useModal from "~/hooks/useModal";
-import GlobalStyle from "~/styles/GlobalStyles";
+import { Global, ThemeProvider } from '@emotion/react'
+import useModal from '~/hooks/useModal'
+import GlobalStyle from '~/styles/GlobalStyles'
 import { default as THEME } from '~/styles/Theme'
 
 export default {
-    title: 'Common/Components/useModal',
-} 
+  title: 'Common/Components/useModal'
+}
 
 export const Default = {
-    render: function Render() {
-        const {Modal,open,close,isOpen} = useModal();
+  render: function Render() {
+    const { Modal, open, close, isOpen, isAnimating } = useModal()
 
     return (
-        <div>
-            <ThemeProvider theme={THEME["dark"]}>
-            <Global styles={GlobalStyle(THEME["dark"])} />
-                <button onClick={open} style={{backgroundColor:"lightblue", padding: "5px", borderRadius: "5px"}}>Modal 오픈</button>
-                <Modal isOpen={isOpen} close={close}>
-                    <div>안녕하세요</div>
-                    <div>하이하이복실</div>
-                </Modal>
-            </ThemeProvider>
-        </div>
+      <div>
+        <ThemeProvider theme={THEME['dark']}>
+          <Global styles={GlobalStyle(THEME['dark'])} />
+          <button
+            onClick={open}
+            style={{
+              backgroundColor: 'lightblue',
+              padding: '5px',
+              borderRadius: '5px'
+            }}>
+            Modal 오픈
+          </button>
+          <Modal
+            isOpen={isOpen}
+            close={close}
+            isAnimating={isAnimating}>
+            <div>안녕하세요</div>
+            <div>하이하이복실</div>
+          </Modal>
+        </ThemeProvider>
+      </div>
     )
-    }
+  }
 }
