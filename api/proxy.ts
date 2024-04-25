5a5c09608ba8d0266c6b1f040415cf0ab89c26b6
신준혁 <oridori2705@naver.com>
2024-04-26 03:00:53 +0900

    refactor: 코드 리팩토링 및 vercel 타입스크립트 버전오류 수정

--- api/proxy.ts
+++ api/proxy.ts
@@ -2,17 +2,16 @@ import axios from 'axios'
 import converter from 'xml-js'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-const apiUrl = process.env.VITE_API_BASE_URL
-const apiKey = process.env.VITE_API_KEY
+const { VITE_API_BASE_URL, VITE_API_KEY } = process.env
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   const sText = req.query.searchData
   const pageNo = req.query.pageNo
   const numOfRows = req.query.numOfRows
 
-  const { data } = await axios.get(apiUrl!, {
+  const { data } = await axios.get(VITE_API_BASE_URL!, {
     params: {
-      apiKey,
+      apiKey: VITE_API_KEY,
       sType: 'sCntntsSj',
       sText,
       pageNo,
@@ -21,5 +20,6 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   })
   const xmlToJson = converter.xml2json(data)
 
+  res.setHeader('Content-Type', 'application/json')
   res.send(xmlToJson)
 }
