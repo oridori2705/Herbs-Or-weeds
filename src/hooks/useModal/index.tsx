8d25291a35720c7d13c0e412e60b66164878f0d7
신준혁 <oridori2705@naver.com>
2024-09-09 21:40:19 +0900

    refactor: Modal 배경 스크롤 방지 기능 추가

--- src/hooks/useModal/index.tsx
+++ src/hooks/useModal/index.tsx
@@ -36,6 +36,19 @@ const ModalComponent = ({
   }
 
   useEffect(() => {
+    document.body.style.cssText = `
+    position: fixed; 
+    top: -${window.scrollY}px;
+    overflow-y: scroll;
+    width: 100%;`
+    return () => {
+      const scrollY = document.body.style.top
+      document.body.style.cssText = ''
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1)
+    }
+  }, [])
+
+  useEffect(() => {
     if (isOpen) {
       setIsAnimating(true)
     }
