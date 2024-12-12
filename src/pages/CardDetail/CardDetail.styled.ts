393e69049558c4a4025c0bc156c8f8f21cf20dcc
신준혁 <oridori2705@naver.com>
2024-12-12 16:33:23 +0900

    fix: br태그 replace로 데이터 정제

--- src/pages/CardDetail/CardDetail.styled.ts
+++ src/pages/CardDetail/CardDetail.styled.ts
@@ -38,6 +38,7 @@ export const InfoItem = styled.p`
   margin: 5px 0;
   font-size: 16px;
   color: #555;
+  white-space: pre-wrap;
 `
 
 export const SectionTitle = styled.h2`
@@ -50,4 +51,5 @@ export const Description = styled.p`
   font-size: 16px;
   color: #333;
   line-height: 1.5;
+  white-space: pre-wrap;
 `
